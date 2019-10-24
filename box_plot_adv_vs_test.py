44e35a8bb24c41dd3a4560dd8890f2e00240c7d0
hvdthong <hvdthong@gmail.com>
2019-10-25 07:28:52 +0800

    update files

--- box_plot_adv_vs_test.py
+++ box_plot_adv_vs_test.py
@@ -23,6 +23,9 @@ if __name__ == '__main__':
     parser.add_argument(
         "--prob", "-prob", help="Confidence score", action="store_true"
     )
+    parser.add_argument(
+        "--prob_no_normalize", "-prob_no_normalize", help="No Normalize: Confidence score", action="store_true"
+    )
 
     args = parser.parse_args()
     assert args.d in ["mnist", "cifar"], "Dataset should be either 'mnist' or 'cifar'"
@@ -121,6 +124,44 @@ if __name__ == '__main__':
             ax.set_xticklabels(['Test', 'Adv'])
             plt.savefig('./figures/prob_incorrect_test_vs_adversarial_{}.jpg'.format(args.d))
 
+        if args.adv_good and args.prob_no_normalize:
+            dsa_test_score = './sa/prob_no_normalize_mnist.txt'
+            dsa_adv_score = './sa/prob_adv_no_normalize_mnist.txt'
+
+            dsa_test_score = load_file(dsa_test_score)
+            dsa_adv_score = load_file(dsa_adv_score)
+
+            dsa_test_score = [float(i) for i in dsa_test_score]
+            dsa_adv_score = [float(i) for i in dsa_adv_score]
+
+            (x_train, y_train), (x_test, y_test) = mnist.load_data()
+            
+            # number of class
+            num_class = 10
+            y_train = utils.to_categorical(y_train, num_class)
+            y_test = utils.to_categorical(y_test, num_class)
+
+            # load adversarial 
+            x_adv = np.load('./adv/adv_mnist.npy')
+
+            # Load pre-trained model.            
+            model = load_model('./model_tracking/model_improvement-04-0.99_mnist.h5')
+            model.summary()
+
+            y_pred_adv = model.predict(x_adv)
+
+            correct, incorrect = mnist_get_correct_and_incorrect_test_images(y_pred=y_pred_adv, y_true=y_test)
+            
+            dsa_test_score = [dsa_test_score[index] for index in incorrect]
+            dsa_adv_score = [dsa_adv_score[index] for index in incorrect]
+
+            data = [dsa_test_score, dsa_adv_score]
+            fig, ax = plt.subplots()
+            ax.set_title('Incorrect-Confidence Score (NN): Test vs Adversarial Examples of {} dataset'.format(args.d))
+            ax.boxplot(data)
+            ax.set_xticklabels(['Test', 'Adv'])
+            plt.savefig('./figures/prob_no_normalize_incorrect_test_vs_adversarial_{}.jpg'.format(args.d))
+
     if args.d == "cifar":
         if args.all:
             dsa_test_score = './sa/dsa_cifar.txt'
@@ -214,3 +255,41 @@ if __name__ == '__main__':
             ax.boxplot(data)
             ax.set_xticklabels(['Test', 'Adv'])
             plt.savefig('./figures/prob_incorrect_test_vs_adversarial_{}.jpg'.format(args.d))
+
+        if args.adv_good and args.prob_no_normalize:
+            dsa_test_score = './sa/prob_no_normalize_cifar.txt'
+            dsa_adv_score = './sa/prob_adv_no_normalize_cifar.txt'
+
+            dsa_test_score = load_file(dsa_test_score)
+            dsa_adv_score = load_file(dsa_adv_score)
+
+            dsa_test_score = [float(i) for i in dsa_test_score]
+            dsa_adv_score = [float(i) for i in dsa_adv_score]
+
+            (x_train, y_train), (x_test, y_test) = mnist.load_data()
+            
+            # number of class
+            num_class = 10
+            y_train = utils.to_categorical(y_train, num_class)
+            y_test = utils.to_categorical(y_test, num_class)
+
+            # load adversarial 
+            x_adv = np.load('./adv/adv_cifar.npy')
+
+            # Load pre-trained model.            
+            model = load_model('./model_tracking/cifar_model_improvement-496-0.87.h5')
+            model.summary()
+
+            y_pred_adv = model.predict(x_adv)
+
+            correct, incorrect = mnist_get_correct_and_incorrect_test_images(y_pred=y_pred_adv, y_true=y_test)
+            
+            dsa_test_score = [dsa_test_score[index] for index in incorrect]
+            dsa_adv_score = [dsa_adv_score[index] for index in incorrect]
+
+            data = [dsa_test_score, dsa_adv_score]
+            fig, ax = plt.subplots()
+            ax.set_title('Incorrect-Confidence Score (NN): Test vs Adversarial Examples of {} dataset'.format(args.d))
+            ax.boxplot(data)
+            ax.set_xticklabels(['Test', 'Adv'])
+            plt.savefig('./figures/prob_no_normalize_incorrect_test_vs_adversarial_{}.jpg'.format(args.d))
